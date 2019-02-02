410491b7462842e2fd694e4a17ce15ed48ef0858
Gerard <gerardbajona@gmail.com>
2019-02-02 17:33:48 +0100

    i3: add keymaps to rotate the screen

---
+++ i3/.config/i3/rotate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Script to rotate the screen.
+# It works through i3wm and xrandr.
+#
+# Full configuration is available in this repository:
+# URL: https://github.com/gerardbm/dotfiles
+
+OUTPUT=$(xrandr | grep '\Wconnected' | awk '{ print $1 }')
+xrandr --output "$OUTPUT" --rotate "$1"
+notify-send "$OUTPUT → Screen rotation: $1"
