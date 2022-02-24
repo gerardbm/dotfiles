974498617f7326d259657b1aa86c3c87e820a24f
Gerard <gerardbajona@gmail.com>
2022-02-24 23:59:02 +0100

    Update cmus playlist.pl

--- cmus/.config/cmus/playlist.pl
+++ cmus/.config/cmus/playlist.pl
@@ -1,6 +1,5 @@
 http://ice.stream.frequence3.net/frequence3-128.mp3
 http://streamer.psyradio.org:8010/
-http://streaming.rac1.cat/
 http://www.radioparadise.com/musiclinks/rp_128aac.m3u
 http://relay2.slayradio.org:8000/
 http://ice1.somafm.com/thetrip-128-mp3
